6e3602bd28a655c73508bfdfb9c395106b2f97af
german <germanjongewaard@gmail.com>
2017-12-20 15:55:30 +0100

    new commit

--- app/src/main/java/dev/com/jongewaard/fragmentsapp/fragments/DataFragment.java
+++ app/src/main/java/dev/com/jongewaard/fragmentsapp/fragments/DataFragment.java
@@ -7,23 +7,62 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import dev.com.jongewaard.fragmentsapp.R;
 
 public class DataFragment extends Fragment {
+
+    private EditText textData;
+    private Button btnSend;
+
+    private DataListener callback;
+
 
     public DataFragment() {
         // Required empty public constructor
     }
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+
+        }catch (Exception e){
+            //el Log es para mostrarme info en el "Logcat"
+            //Log.i(e.toString());
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
     //
-        View view = inflater.inflate(R.layout.fragment_data, container, false); 
+        View view = inflater.inflate(R.layout.fragment_data, container, false);
+        //Lógica para capturar los elementos de la UI
 
-        // Inflate the layout for this fragment
+
+        textData = (EditText)view.findViewById(R.id.editTextData);
+        btnSend = (Button)view.findViewById(R.id.buttonSend);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendText("");
+            }
+        });
+
         return view;
+    }
+
+    private void sendText(String text) {
+
+    }
+
+    //método que comunica
+    public interface DataListener{
+        void sendData();
     }
 
 }
