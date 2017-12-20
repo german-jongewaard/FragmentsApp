cbf94872c62d47a069c841bae5a51b3cbbcc090c
german <germanjongewaard@gmail.com>
2017-12-21 00:28:22 +0100

    new commit

--- app/src/main/java/dev/com/jongewaard/fragmentsapp/activities/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/fragmentsapp/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package dev.com.jongewaard.fragmentsapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -9,27 +10,44 @@ import dev.com.jongewaard.fragmentsapp.fragments.DetailsFragment;
 
 public class MainActivity extends FragmentActivity implements DataFragment.DataListener {
 
+    private boolean isMultiPanel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setMultiPanel();
     }
 
     @Override
     public void sendData(String text) {
-        /*
-        Llamar al método renderizarTexto del DetailsFragment, pasando el texto
-        que recibimos por el parámetro, en este mismo método.
-        */
-        //aqui creo una instancia del fragmento al que quiero comunicarme
-            /*El findFragmentById me trae un Fragment GENERICO por eso le hago el Casting
-             *con DetailsFragments
-             *No creo un fragment, lo encuentro, lo traigo con el findFragmentById
-             * */
-        DetailsFragment detailsFragment = (DetailsFragment)getSupportFragmentManager()
-                .findFragmentById(R.id.detailsFragment);
 
-        detailsFragment.renderText(text);
+         if(isMultiPanel){
+/* Llamar al método renderizarTexto del DetailsFragment, pasando el texto que recibimos por el
+parámetro, en este mismo método. */
+//aqui creo una instancia del fragmento al que quiero comunicarme
+/* El findFragmentById me trae un Fragment GENERICO por eso le hago el Casting con DetailsFragments
+No creo un fragment, lo encuentro, lo traigo con el findFragmentById */
+             DetailsFragment detailsFragment = (DetailsFragment)getSupportFragmentManager()
+                     .findFragmentById(R.id.detailsFragment);
+             detailsFragment.renderText(text);
+         } else {
 
+             Intent intent = new Intent(this, DetailsActivity.class);
+             intent.putExtra("text", text);
+             startActivity(intent);
+         }
+
+
+
+
+
+
+    }
+
+    private void setMultiPanel(){
+            //Si tengo dos Fragment va a dar verdadero, si solo tiene uno va a dar falso
+        isMultiPanel = (getSupportFragmentManager().findFragmentById(R.id.detailsFragment) != null);
     }
 }
